56f15a75e7bfa116c4324ba87780bc9b9691ac69
Pijush Ghosh <44667071+pijushghosh014@users.noreply.github.com>
2018-11-05 14:55:01 +0530

    Add files via upload
    Need to do: 
    1. Install Blynk App to your mobile. URL given bellow:
    https://play.google.com/store/apps/details?id=cc.blynk&hl=en
    Create your account. then create your project on Blynk app. you will get a Auth token ID in your register mail id by Blynk App.
    
    2. For the Google assistant , Go to IFTTT site (ifttt.com). Click on Google Assistant, create 1st Applet and webhooks for turn on light condition then create 2nd Applet and webhooks for turn off light condition. 
    You need this URL for Make a web request : http://188.166.206.43/put your auth token id/update/D14
    (D14 for NodeMCU digital pin D5, you can modify as per your coding)

---
+++ NodeMCU_Blynk_GoogleAssistant.ino
@@ -0,0 +1,29 @@
+
+#define BLYNK_PRINT Serial
+
+
+#include <ESP8266WiFi.h>
+#include <BlynkSimpleEsp8266_SSL.h>
+
+// You should get Auth Token in the Blynk App.
+// Go to the Project Settings (nut icon).
+char auth[] = "588340eb3ca645378180f55e90b75c0e"; // Auth tokenID send by Blynk App to your mail ID when you create your perticular project  
+
+// Your WiFi credentials.
+// Set password to "" for open networks.
+char ssid[] = "PIJUSH_HP6GEN"; // Put your WiFi SSID
+char pass[] = "pijushhp6gen"; //Put your WiFi password
+
+void setup()
+{
+  // Debug console
+  Serial.begin(115200);
+
+  Blynk.begin(auth, ssid, pass);
+}
+
+void loop()
+{
+  Blynk.run();
+}
+
